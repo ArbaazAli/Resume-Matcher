d3c736a8fc76e256afd1e417e87c9fbb31cee3bc
ArbaazAli <arbajali02@gmail.com>
2020-11-25 15:10:36 +0530

    debug false

--- jdm/settings.py
+++ jdm/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wg67+&h9s=*x+q()%#rl^)pjcp(pbd-3cwwxn2)++o*=^w9m3g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'resume-matcher-web.herokuapp.com',]
 
 
 # Application definition
@@ -120,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
