48f6bcefdbcfe071d17f4876d0d8f761936aa30f
ArbaazAli <arbajali02@gmail.com>
2020-11-25 14:32:39 +0530

    added heroku settings

--- jdm/settings.py
+++ jdm/settings.py
@@ -123,4 +123,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.joi
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
